8844438e90532cde0dbf47b35c2f4dd6a6f55b42
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-04 18:03:57 -0300

    feat: Add campaigns query resolver

--- modules/graphql/resolvers/query.ts
+++ modules/graphql/resolvers/query.ts
@@ -1,8 +1,21 @@
 import { ApolloContext } from "../../../interfaces/apollo";
+import Campaign from "../../firebase/entities/Campaign";
+import User from "../../firebase/entities/User";
 
 const queryResolver = {
   _user: (_parent: unknown, _: unknown, ctx: ApolloContext) => {
     return ctx.user;
+  },
+  campaigns: async (_parent: unknown, _: unknown, ctx: ApolloContext) => {
+    if (!ctx.user) return;
+
+    const userRef = await User.getRef(ctx.user.uid);
+    const campaigns = await Campaign.getByUser(userRef);
+    const parsedCampaigns = await Promise.all(
+      campaigns.map(async (campaign) => Campaign.parse(campaign!))
+    );
+
+    return parsedCampaigns;
   },
 };
 
