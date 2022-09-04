6105324330bed9103e3e8c31f7ab8b9dd2471b5b
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-03 22:36:13 -0300

    feat: Add apollo and auth providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,47 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+import { auth } from "../libs/firebaseApp";
+import { setContext } from "@apollo/client/link/context";
+import AuthProvider from "../context/AuthProvider";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:3000/api/graphql",
+});
+
+const getIdToken = async () => await auth.currentUser?.getIdToken();
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await getIdToken();
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+      "Bypass-Tunnel-Reminder": true,
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: ApolloLink.from([authLink, httpLink]),
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
