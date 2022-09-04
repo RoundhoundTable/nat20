ba15e3d5e7e2f15f092455cf6eae8e9a8ef7ad2e
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-03 22:29:25 -0300

    feat: Add auth mutations and input types

--- modules/graphql/schema.ts
+++ modules/graphql/schema.ts
@@ -2,7 +2,29 @@ import { gql } from "apollo-server-micro";
 
 const typeDefs = gql`
   type Query {
-    _dummy: String
+    _user: User
+  }
+
+  type Mutation {
+    register(credentials: RegisterMutationInput!): String
+    login(credentials: LoginMutationInput!): String
+  }
+
+  type User {
+    uid: String
+    username: String
+  }
+
+  input RegisterMutationInput {
+    email: String!
+    username: String!
+    password: String!
+    confirmPassword: String!
+  }
+
+  input LoginMutationInput {
+    email: String!
+    password: String!
   }
 `;
 
