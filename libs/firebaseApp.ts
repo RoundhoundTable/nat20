ec10f0636b8333baa85ddd5ac7fe572aa074dba5
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-03 22:34:26 -0300

    feat: Add firebase app auth

---
+++ libs/firebaseApp.ts
@@ -0,0 +1,19 @@
+import { getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+if (!getApps().length) {
+  try {
+    initializeApp({
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_ID,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const auth = getAuth();
