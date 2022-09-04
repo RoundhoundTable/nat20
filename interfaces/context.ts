aa1d19614b50bc8a919efda272fb37a56f6d02e6
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-04 16:19:56 -0300

    feat: Add socket context interface

--- interfaces/context.ts
+++ interfaces/context.ts
@@ -1,4 +1,5 @@
 import type { UserCredential } from "firebase/auth";
+import { Socket } from "socket.io-client";
 import { IUser } from "./firebase";
 
 export interface IAuthContext {
@@ -7,4 +8,9 @@ export interface IAuthContext {
   signOut: () => void;
   signUp: (token: string) => Promise<UserCredential>;
   fetch: () => Promise<void>;
+}
+
+export interface ISocketContext {
+  socket: Socket | null;
+  initializeSocket: () => Promise<void>;
 }
