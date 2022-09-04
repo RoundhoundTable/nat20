78efc1cf213d87c61df8bf338c25b57569ec691b
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-03 22:35:28 -0300

    feat: Add context and resolvers

--- libs/apollo.ts
+++ libs/apollo.ts
@@ -1,9 +1,17 @@
 import { ApolloServer } from "apollo-server-micro";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import typeDefs from "../modules/graphql/schema";
+import Mutation from "../modules/graphql/resolvers/mutation";
+import Query from "../modules/graphql/resolvers/query";
+import { createContext } from "../modules/graphql/context";
 
 const apollo: ApolloServer = new ApolloServer({
   typeDefs,
+  resolvers: {
+    Query,
+    Mutation,
+  },
+  context: createContext,
   plugins: [
     ApolloServerPluginLandingPageGraphQLPlayground({
       settings: {
