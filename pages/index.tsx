b411e78ee1ca699ece1dffbd31750271ea514eae
Ignun <bajunazu@gmail.com>
2022-09-08 18:57:44 -0300

    Feat: add components to the main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,20 @@
 import type { NextPage } from 'next'
+import Image from 'next/image';
+import Logo from '../assets/images/logo.svg'
+import Button from '../components/Button';
 
-const Home: NextPage = () => <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+const Home: NextPage = () => {
+    
+
+    return (
+        <div className="flex flex-col gap-28 p-5 bg-[#343642]">
+            <Image src={Logo} />
+            <div className="flex flex-col p-10 gap-5 justify items-center">
+                <Button>Login</Button>
+                <Button>Register</Button>
+            </div>
+        </div>
+    )
+};
 
 export default Home
