39196fa737d9d220b2613f8fd0de286f2067fdf2
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-08 20:47:39 -0300

    fix: Div height and center logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,18 @@
-import type { NextPage } from 'next'
-import Image from 'next/image';
-import Logo from '../assets/images/logo.svg'
-import Button from '../components/Button';
+import type { NextPage } from "next";
+import Image from "next/image";
+import Logo from "../assets/images/logo.svg";
+import Button from "../components/Button";
 
 const Home: NextPage = () => {
-    
-
-    return (
-        <div className="flex flex-col gap-28 p-5 bg-[#343642]">
-            <Image src={Logo} />
-            <div className="flex flex-col p-10 gap-5 justify items-center">
-                <Button>Login</Button>
-                <Button>Register</Button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col p-5 bg-[#343642] h-screen justify-center">
+      <Image src={Logo} />
+      <div className="mt-28 flex flex-col p-10 gap-5 items-center">
+        <Button>Login</Button>
+        <Button>Register</Button>
+      </div>
+    </div>
+  );
 };
 
-export default Home
+export default Home;
