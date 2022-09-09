e7b8081e1a6ca9f08a352db3324935887080cc7d
01R4D-M <monterosrubendario@gmail.com>
2022-09-09 20:45:22 -0300

    feat: Add Character List component to the main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import type { NextPage } from "next";
 import Image from "next/image";
 import Logo from "../assets/images/logo.svg";
 import Button from "../components/Button";
+import { CharactersList } from "../components/CharactersList";
 
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col p-5 bg-[#343642] h-screen justify-center">
+    <CharactersList/>
       <Image src={Logo} />
       <div className="mt-28 flex flex-col p-10 gap-5 items-center">
         <Button>Login</Button>
