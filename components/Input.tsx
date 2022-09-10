32c945bbbba5fc343f6378229bc57b4e6c94b0c8
unknown <lucas.cruz2003@hotmail.com>
2022-09-09 21:36:15 -0300

    mod: modified Input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -6,9 +6,11 @@ export const Input: NextComponentType = () => {
       <div>
         <input
           type="text"
-          className="bg-black/30 text-white border-b-2 border-[#F6E3B9] px-2 rounded-md focus:outline-none"
+          className="bg-black/30 text-white border-b-2 border-[#F6E3B9] px-2 sm:w-80 sm:h-9 w-60 rounded-lg focus:outline-none"
         />
       </div>
     </>
   );
 };
+
+export default Input;
