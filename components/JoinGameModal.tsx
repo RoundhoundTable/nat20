03c7780fcf0cd1469554b0523f2be7b1dc8c64b9
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-27 20:51:52 -0300

    refactor: Adjust to new input component

--- components/JoinGameModal.tsx
+++ components/JoinGameModal.tsx
@@ -1,19 +1,13 @@
 import { NextComponentType } from "next";
-import Input from "./Input";
+import { Input } from "./Input";
 import Button from "../components/Button";
 
 const JoinGameModal: NextComponentType = () => {
   return (
-    <div className="flex flex-col items-center gap-5 text-white">
-      <p className="text-2xl font-bold uppercase">Join Game</p>
-      <div>
-        <p className="font-medium">Game ID</p>
-        <Input />
-      </div>
-      <div>
-        <p className="font-medium">Password</p>
-        <Input />
-      </div>
+    <div className="flex flex-col items-center gap-5">
+      <p className="text-2xl font-bold uppercase text-primary-500">Join Game</p>
+      <Input name="id" label="Game ID" type={"text"} />
+      <Input name="password" label="Password" type={"password"} />
       <Button>Join</Button>
     </div>
   );
