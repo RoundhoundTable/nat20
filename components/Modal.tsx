f50816c39de1c52eac5431ced9a0b0ba49045900
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-27 19:48:17 -0300

    fix: Styling and structure

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,15 +1,42 @@
-import { ReactNode } from "react";
+import { Icon } from "@iconify/react";
+import { FunctionComponent, ReactNode } from "react";
+import { EModals } from "../enums/modals";
+import useModal from "../hooks/useModal";
+import JoinGameModal from "./JoinGameModal";
 
-const Modal = ({ children }: { children: ReactNode }) => {
+interface IModalProps {
+  modal: EModals;
+}
+
+const modals: Record<EModals, ReactNode> = {
+  JOIN_GAME: <JoinGameModal />,
+  REGISTER: <></>,
+  LOGIN: <></>,
+};
+
+const Modal: FunctionComponent<IModalProps> = ({ modal }) => {
+  const { unsetModal } = useModal();
   return (
-    <div className="flex justify-center">
-      <div className="bg-[#44465A] mt-10 mx-28 h-fit max-w-fit min-w-fit border-2 rounded-lg flex">
-        <div className="bg-[#252735] p-5 m-5 h-fit text-white rounded-sm flex content-center">
-          <div className="p-3 flex-row justify-content-center">{children}</div>
-        </div>
-      </div>
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#252735] p-8 border-4 border-[#44465A] z-50">
+      <Icon
+        onClick={unsetModal}
+        className="absolute right-0 top-0 mr-2 mt-2"
+        color="#F6E3B9"
+        icon="akar-icons:cross"
+      />
+      {modals[modal]}
     </div>
   );
 };
-
+/*
+    <div className="absolute top-0 left-0 ml-1/2">
+      <div className=" mx-28 h-fit max-w-fit min-w-fit border-2 rounded-lg flex">
+        <div className=" p-5 m-5 h-fit text-white rounded-sm flex content-center">
+          <div className="p-3 flex-row justify-content-center">
+            
+          </div>
+        </div>
+      </div>
+      
+    </div>*/
 export default Modal;
