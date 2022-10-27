7fb3e3f50ce503f26247faeb419b219168724ccb
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-27 19:47:47 -0300

    feat: Add ButtonHTMLAttributes props

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,16 @@
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, FunctionComponent, ReactNode } from "react";
 
-const Button = ({children}: {children: ReactNode}) => {
-    return (
-        <button className="bg-primary-500 sm:w-80 sm:h-9 w-60 rounded-lg uppercase border border-primary-500 hover:bg-background-500 text-background-500 hover:text-primary-500 text-xl">{children}</button>
-    )
-}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button: FunctionComponent<ButtonProps> = ({ children, ...props }) => {
+  return (
+    <button
+      {...props}
+      className="bg-primary-500 sm:w-80 sm:h-9 w-60 rounded-lg uppercase border border-primary-500 hover:bg-background-500 text-background-500 hover:text-primary-500 text-xl"
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
