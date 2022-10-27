fa9ed98b8947f484198005bfe901bbe285507395
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-27 20:07:05 -0300

    refactor: Change from firebase database to prisma orm

--- modules/graphql/resolvers/query.ts
+++ modules/graphql/resolvers/query.ts
@@ -1,6 +1,4 @@
 import { ApolloContext } from "../../../interfaces/apollo";
-import Campaign from "../../firebase/entities/Campaign";
-import User from "../../firebase/entities/User";
 
 const queryResolver = {
   _user: (_parent: unknown, _: unknown, ctx: ApolloContext) => {
@@ -9,13 +7,13 @@ const queryResolver = {
   campaigns: async (_parent: unknown, _: unknown, ctx: ApolloContext) => {
     if (!ctx.user) return;
 
-    const userRef = await User.getRef(ctx.user.uid);
-    const campaigns = await Campaign.getByUser(userRef);
-    const parsedCampaigns = await Promise.all(
-      campaigns.map(async (campaign) => Campaign.parse(campaign!))
-    );
+    const campaigns = await ctx.prisma.campaign.findMany({
+      where: {
+        dungeonMasterId: ctx.user.id,
+      },
+    });
 
-    return parsedCampaigns;
+    return campaigns;
   },
 };
 
