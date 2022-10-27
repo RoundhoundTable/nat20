a79bb784b377f6c9c7d27aae3c6510b261261ea6
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-27 19:57:50 -0300

    refactor: Remove firebase database

--- libs/firebaseAdmin.ts
+++ libs/firebaseAdmin.ts
@@ -1,7 +1,6 @@
 import admin, { ServiceAccount } from "firebase-admin";
 import serviceAccount from "../modules/firebase/service-account.json";
 import { getAuth } from "firebase-admin/auth";
-import { getFirestore } from "firebase-admin/firestore";
 
 if (!admin.apps.length) {
   try {
@@ -14,4 +13,3 @@ if (!admin.apps.length) {
 }
 
 export const auth = getAuth();
-export const database = getFirestore();
