dd1c4f9b3df7f5464169d489e4c1179d42639f41
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-27 20:09:38 -0300

    feat: Add socket listeners

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,20 +1,27 @@
 import { Server, Socket } from "socket.io";
+import createRoom from "../../modules/sockets/handlers/createRoom";
+import disconnect from "../../modules/sockets/handlers/disconnect";
+import joinRoom from "../../modules/sockets/handlers/joinRoom";
 
-const SocketHandler = (req: any, res: any) => {
+export default function handler(req: any, res: any) {
   if (res.socket.server.io) {
     res.end();
     return;
   }
 
   const io = new Server(res.socket.server);
+  res.socket.server.io = io;
 
-  const onConnection = (socket: Socket) => {
-    console.log("Socket Connected!");
+  const onConnection = async (socket: Socket) => {
+    socket.on("joinRoom", joinRoom);
+    socket.on("createRoom", createRoom);
+    socket.on("sendMessage", () => {});
+    socket.on("newMessage", () => {});
+    socket.on("disconnect", disconnect);
   };
 
   io.on("connection", onConnection);
 
   res.end();
-};
-
-export default SocketHandler;
+  return;
+}
