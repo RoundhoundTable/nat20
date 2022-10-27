c322dffd93cbe2cab1c0d3be3ff6d97c9ef00bd8
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-27 19:48:53 -0300

    feat: Add Modal context provider

---
+++ context/ModalProvider.tsx
@@ -0,0 +1,30 @@
+import { createContext, PropsWithChildren, useState } from "react";
+import { EModals } from "../enums/modals";
+import { IModalContext } from "../interfaces/context";
+import Modal from "../components/Modal";
+
+export const ModalContext = createContext<IModalContext | null>(null);
+
+const ModalProvider = ({ children }: PropsWithChildren<any>) => {
+  const [modal, setModalState] = useState<EModals | null>(null);
+
+  const unsetModal = () => setModalState(null);
+  const setModal = (modal: EModals) => {
+    setModalState(modal);
+  };
+
+  const value = {
+    modal,
+    setModal,
+    unsetModal,
+  };
+
+  return (
+    <ModalContext.Provider value={value}>
+      {modal && <Modal modal={modal} />}
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+export default ModalProvider;
