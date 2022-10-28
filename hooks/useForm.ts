734a431b0f5c8325436dd40a66bc14598d806809
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-27 22:12:47 -0300

    feat: Add errors and submit

--- hooks/useForm.ts
+++ hooks/useForm.ts
@@ -1,8 +1,13 @@
 import { ApolloError } from "@apollo/client";
 import { useState } from "react";
 
-export const useForm = <T>(initialValue?: T) => {
+export const useForm = <T>({ initialValue }: { initialValue?: T }) => {
   const [form, setForm] = useState<T>(initialValue as T);
+  const [errors, setErrors] = useState<T | null>();
+
+  const onError = (error: ApolloError) => {
+    setErrors({ ...errors, ...error } as T);
+  };
 
   const onChange = (ev: any) =>
     setForm({
@@ -10,14 +15,40 @@ export const useForm = <T>(initialValue?: T) => {
       [ev.target.name]: ev.target.value,
     } as T);
 
+  const resetErrors = () => {
+    setErrors(null);
+  };
+
   const updateForm = (newForm: T) => setForm({ ...form, ...newForm });
 
   const resetForm = () => setForm(initialValue as T);
+
+  const submit = async ({
+    ev,
+    func,
+  }: {
+    ev?: any;
+    func: (...args: any) => any;
+  }) => {
+    try {
+      if (ev) ev.preventDefault();
+
+      resetErrors();
+
+      func();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return {
     form,
     resetForm,
     updateForm,
     onChange,
+    errors,
+    resetErrors,
+    onError,
+    submit,
   };
 };
