d7ed82b99936ad3ac4fed73084be323ca136e806
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-27 22:13:11 -0300

    feat: Add functionality

--- components/LoginModal.tsx
+++ components/LoginModal.tsx
@@ -1,14 +1,73 @@
 import { NextComponentType } from "next";
 import { Input } from "./Input";
 import Button from "../components/Button";
+import { useEffect } from "react";
+import Router from "next/router";
+import { useAuth } from "../hooks/useAuth";
+import { gql, useMutation } from "@apollo/client";
+import { useForm } from "../hooks/useForm";
+
+interface LoginForm {
+  email: string;
+  password: string;
+}
 
 const LoginModal: NextComponentType = () => {
+  const { signIn } = useAuth();
+  const { form, onChange, errors, onError, submit } = useForm<LoginForm>({});
+  const [Login, { data }] = useMutation(
+    gql`
+      mutation Mutation($credentials: LoginMutationInput!) {
+        login(credentials: $credentials)
+      }
+    `,
+    {
+      onError,
+    }
+  );
+
+  const loginWrapper = async () => {
+    await Login({
+      variables: {
+        credentials: {
+          ...form,
+        },
+      },
+    });
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (data) {
+        await signIn(data.login);
+        Router.reload();
+      }
+    })();
+  }, [data]);
+
   return (
     <div className="flex flex-col items-center gap-5 text-white">
       <p className="text-2xl font-bold uppercase text-primary-500">Login</p>
-      <Input name="email" label="Email" type={"email"} />
-      <Input name="password" label="Password" type={"password"} />
-      <Button>Sign In</Button>
+      <form
+        method="POST"
+        onSubmit={(ev: any) => submit({ ev, func: loginWrapper })}
+      >
+        <Input
+          name="email"
+          label="Email"
+          type={"email"}
+          onChange={onChange}
+          error={{ message: errors?.email }}
+        />
+        <Input
+          name="password"
+          label="Password"
+          type={"password"}
+          onChange={onChange}
+          error={{ message: errors?.password }}
+        />
+        <Button type="submit">Sign In</Button>
+      </form>
     </div>
   );
 };
--- components/RegisterModal.tsx
+++ components/RegisterModal.tsx
@@ -1,20 +1,90 @@
 import { NextComponentType } from "next";
 import { Input } from "./Input";
 import Button from "./Button";
+import { useAuth } from "../hooks/useAuth";
+import { useForm } from "../hooks/useForm";
+import { gql } from "apollo-server-core";
+import { useMutation } from "@apollo/client";
+import { useEffect } from "react";
+import Router from "next/router";
+
+interface RegisterForm {
+  email: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
+}
 
 const RegisterModal: NextComponentType = () => {
+  const { signUp } = useAuth();
+  const { form, onChange, errors, onError, submit } = useForm<RegisterForm>({});
+  const [Register, { data }] = useMutation(
+    gql`
+      mutation Mutation($credentials: RegisterMutationInput!) {
+        register(credentials: $credentials)
+      }
+    `,
+    {
+      onError,
+    }
+  );
+
+  const registerWrapper = async () => {
+    await Register({
+      variables: {
+        credentials: {
+          ...form,
+        },
+      },
+    });
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (data) {
+        await signUp(data.register);
+        Router.reload();
+      }
+    })();
+  }, [data]);
+
   return (
     <div className="flex flex-col items-center gap-5 text-white">
       <p className="text-2xl font-bold uppercase text-primary-500">Register</p>
-      <Input name="email" label="Email" type={"email"} />
-      <Input name="username" label="Username" type={"text"} />
-      <Input name="password" label="Password" type={"password"} />
-      <Input
-        name="confirmPassword"
-        label="Confirm password"
-        type={"password"}
-      />
-      <Button>Sign Up</Button>
+      <form
+        method="POST"
+        onSubmit={(ev: any) => submit({ ev, func: registerWrapper })}
+      >
+        <Input
+          name="email"
+          label="Email"
+          type={"email"}
+          onChange={onChange}
+          error={{ message: errors?.email }}
+        />
+        <Input
+          name="username"
+          label="Username"
+          type={"text"}
+          onChange={onChange}
+          error={{ message: errors?.username }}
+        />
+        <Input
+          name="password"
+          label="Password"
+          type={"password"}
+          onChange={onChange}
+          error={{ message: errors?.password }}
+        />
+        <Input
+          name="confirmPassword"
+          label="Confirm password"
+          type={"password"}
+          onChange={onChange}
+          error={{ message: errors?.confirmPassword }}
+        />
+        <Button type="submit">Sign Up</Button>
+      </form>
     </div>
   );
 };
