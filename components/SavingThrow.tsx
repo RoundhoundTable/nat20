31cce242bcd3f60ddf939c24cb952d69a951ab7a
01R4D-M <monterosrubendario@gmail.com>
2022-11-03 19:02:41 -0300

    refactor: Add conditional render for icons

--- components/SavingThrow.tsx
+++ components/SavingThrow.tsx
@@ -1,14 +1,31 @@
 import { NextComponentType } from "next";
 import { Icon } from "@iconify/react";
+import { FunctionComponent } from "react";
 
-export const SavingThrow:NextComponentType = () => {
+interface ISavingThrowData {
+    throws: (boolean | null)[]
+}
+
+export const SavingThrow: FunctionComponent<ISavingThrowData> = (props) => {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col items-center">
                 <span className="text-sm">Death Saves</span>
-                <div className="flex flex-row">
-                    <Icon icon="fa6-solid:cross" className="drop-shadow-[0px_0px_2px_#F6E3B9]"/>
-                    <Icon icon="mdi:grave-stone"/>
-                    <Icon icon="mdi:grave-stone"/>
+                <div className="flex flex-row justify-center">
+                    {
+                        props.throws[0]
+                        ? <Icon icon="fa6-solid:cross" className="drop-shadow-[0px_0px_2px_#F6E3B9]"/>
+                        : <Icon icon="mdi:grave-stone"/>
+                    }
+                    {
+                        props.throws[1]
+                        ? <Icon icon="fa6-solid:cross" className="drop-shadow-[0px_0px_2px_#F6E3B9]"/>
+                        : <Icon icon="mdi:grave-stone"/>
+                    }
+                    {
+                        props.throws[2]
+                        ? <Icon icon="fa6-solid:cross" className="drop-shadow-[0px_0px_2px_#F6E3B9]"/>
+                        : <Icon icon="mdi:grave-stone"/>
+                    }
                 </div>
         </div>
     )
