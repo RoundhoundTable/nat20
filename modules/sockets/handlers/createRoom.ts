115e1144583daba4503d3236232b9b8b50c2fe2d
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-04 23:01:42 -0300

    refactor: Remove CampaignCharacter and change Emit

--- modules/sockets/handlers/createRoom.ts
+++ modules/sockets/handlers/createRoom.ts
@@ -2,34 +2,25 @@ import { Socket } from "socket.io";
 import SocketHandler from "../../../libs/socket";
 import Room from "../classes/Room";
 import { customAlphabet } from "nanoid";
-import prisma from "../../../libs/prisma";
+
 const genId = customAlphabet(
   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 );
 
-async function createRoom(this: Socket, campaignId: string) {
+async function createRoom(this: Socket, password: string) {
   const id = genId(8);
   const user = this.handshake.auth;
 
-  const campaign = await prisma.campaign.findFirst({
-    where: {
-      dungeonMasterId: user.uid,
-      id: campaignId,
-    },
-  });
+  if (!user) throw new Error("Forbidden");
 
-  if (!campaign) throw new Error("Campaign Not Found");
-
-  const room = new Room(id, campaign);
+  const room = new Room(id, password);
 
   room.joinAsDm(this);
 
   SocketHandler.rooms.set(id, room);
   SocketHandler.socketRooms.set(this, room);
 
-  console.log({ roomId: room.id });
-
-  return true;
+  this.emit("roomConnected", { room: room.id, isDm: true });
 }
 
 export default createRoom;
