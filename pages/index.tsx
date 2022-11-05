b286e72e4e2318893d2689ff41ad090ad6d9ce21
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-04 23:00:06 -0300

    feat: Add socket initialization

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,28 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect } from "react";
 import Logo from "../assets/images/logo.svg";
 import Button from "../components/Button";
 import { CharactersList } from "../components/CharactersList";
 import { EModals } from "../enums/modals";
 import { useAuth } from "../hooks/useAuth";
 import useModal from "../hooks/useModal";
+import { useSocket } from "../hooks/useSocket";
 
 const Home: NextPage = () => {
   const { setModal } = useModal();
   const { currentUser } = useAuth();
+  const { socket, initializeSocket } = useSocket();
+
+  useEffect(() => {
+    (async () => {
+      if (!socket && currentUser) {
+        await initializeSocket();
+      }
+    })();
+  }, []);
+
   return (
     <>
       <Head>
@@ -24,7 +36,9 @@ const Home: NextPage = () => {
             <Button onClick={() => setModal(EModals.JOIN_GAME)}>
               Join Game
             </Button>
-            <Button>Create Game</Button>
+            <Button onClick={() => setModal(EModals.CREATE_GAME)}>
+              Create Game
+            </Button>
           </div>
         ) : (
           <div className="flex flex-col pb-10 gap-5 items-center">
