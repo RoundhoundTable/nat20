1408057c65ccf7d355297bc1cb0e0b2788ecee47
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-05 20:19:45 -0300

    feat: Add id prop + delete character functionality

--- components/Character.tsx
+++ components/Character.tsx
@@ -1,28 +1,49 @@
 import { Icon } from "@iconify/react";
 import { FunctionComponent } from "react";
+import { EModals } from "../enums/modals";
+import { useDeleteCharacter } from "../hooks/useDeleteCharacter";
+import useModal from "../hooks/useModal";
 
 interface ICharacterData {
-    picture: string
-    name: string
-    _class: string
-    level: number
-
+  id: string;
+  picture: string;
+  name: string;
+  _class: string;
+  level: number;
 }
 
-export const Character: FunctionComponent<ICharacterData> = ({picture, name, _class, level}) => {
-    return (
-        <div className="flex flex-row gap-4 items-center p-2 bg-background-600/50 snap-start text-primary-500 relative">
-            <button>
-                <Icon icon="akar-icons:cross" className="absolute top-4 right-2" />
-            </button>
-            <img src={picture} className="w-14 rounded-full object-cover aspect-square"/>
-            <div className="flex flex-col w-1/2">
-                <span className="font-bold">{name}</span>
-                <div className="flex flex-row text-sm justify-between">
-                    <span>{_class}</span>
-                    <span className="drop-shadow-[0px_0px_2px_#F6E3B9]">lv.{level}</span>
-                </div>
-            </div>
+export const Character: FunctionComponent<ICharacterData> = ({
+  id,
+  picture,
+  name,
+  _class,
+  level,
+}) => {
+  const { setModal } = useModal();
+  const { deleteCharacter } = useDeleteCharacter(id);
+  return (
+    <div className="flex flex-row gap-4 items-center p-2 bg-background-600/50 snap-start text-primary-500 relative">
+      <button
+        onClick={() =>
+          setModal(EModals.CONFIRMATION, {
+            message: `¿Está seguro que desea eliminar a ${name}?`,
+            accept: deleteCharacter,
+          })
+        }
+      >
+        <Icon icon="akar-icons:cross" className="absolute top-4 right-2" />
+      </button>
+      <img
+        src={picture}
+        className="w-14 rounded-full object-cover aspect-square"
+      />
+      <div className="flex flex-col w-1/2">
+        <span className="font-bold">{name}</span>
+        <div className="flex flex-row text-sm justify-between">
+          <span>{_class}</span>
+          <span className="drop-shadow-[0px_0px_2px_#F6E3B9]">lv.{level}</span>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
