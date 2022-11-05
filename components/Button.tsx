0bcd72d5b4cc7a830ca2fcd83932e2d7565362ec
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-05 20:17:59 -0300

    feat: Add className merge

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,20 @@
 import { ButtonHTMLAttributes, FunctionComponent, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const Button: FunctionComponent<ButtonProps> = ({ children, ...props }) => {
+const Button: FunctionComponent<ButtonProps> = ({
+  children,
+  className,
+  ...props
+}) => {
   return (
     <button
       {...props}
-      className="bg-primary-500 sm:w-80 sm:h-9 w-60 rounded-lg uppercase border border-primary-500 hover:bg-background-500 text-background-500 hover:text-primary-500 text-xl"
+      className={twMerge(
+        "bg-primary-500 sm:w-80 sm:h-9 w-60 rounded-lg uppercase border border-primary-500 hover:bg-background-500 text-background-500 hover:text-primary-500 text-xl",
+        className
+      )}
     >
       {children}
     </button>
