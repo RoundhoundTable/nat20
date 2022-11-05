5797770e195e040014b11dfb642781d31cbed8cf
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-04 23:04:26 -0300

    feat: Add Game context

--- interfaces/context.ts
+++ interfaces/context.ts
@@ -2,6 +2,7 @@ import type { UserCredential } from "firebase/auth";
 import { Socket } from "socket.io-client";
 import { IUser } from "./entities";
 import { EModals } from "../enums/modals";
+import { Dispatch, SetStateAction } from "react";
 
 export interface IAuthContext {
   currentUser: IUser | null;
@@ -15,7 +16,15 @@ export interface ISocketContext {
   socket: Socket | null;
   initializeSocket: () => Promise<void>;
   joinRoom: (id: string, password: string) => void;
-  createRoom: (campaignId: string) => void;
+  createRoom: (password: string) => void;
+}
+
+export interface IGameContext {
+  roomId: string;
+  isDm: boolean;
+  setIsDm: Dispatch<SetStateAction<boolean>>;
+  setRoomId: Dispatch<SetStateAction<string>>;
+  setIngame: Dispatch<SetStateAction<boolean>>;
 }
 
 export interface IModalContext {
