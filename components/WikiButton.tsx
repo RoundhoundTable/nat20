d5ca74f34f12104460c89dc4dac0f30c05133d17
Ignun <bajunazu@gmail.com>
2022-11-11 19:44:26 -0300

    Refactor: add wikimodal to onclick  wikibutton

--- components/WikiButton.tsx
+++ components/WikiButton.tsx
@@ -1,13 +1,19 @@
 import { Icon } from "@iconify/react";
 import { ButtonHTMLAttributes, FunctionComponent, ReactNode } from "react";
+import useModal from "../hooks/useModal";
+import { EModals } from "../enums/modals";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
 const WikiButton: FunctionComponent<ButtonProps> = ({ children, ...props }) => {
+
+  const { setModal } = useModal();
+
   return (
     <button
       {...props}
       className="flex flex-row items-center p-2 capitalize hover:bg-black/40 text-primary-500 bg-black/30 gap-1"
+      onClick={() => setModal(EModals.WIKI)}
     >
       <Icon icon="ant-design:search-outlined" />
       {children}
