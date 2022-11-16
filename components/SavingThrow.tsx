2728c4798e664a8364d3de9b3b895a270a4a0334
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-15 21:10:14 -0300

    refactor: Improve code

--- components/SavingThrow.tsx
+++ components/SavingThrow.tsx
@@ -1,32 +1,27 @@
-import { NextComponentType } from "next";
 import { Icon } from "@iconify/react";
 import { FunctionComponent } from "react";
 
 interface ISavingThrowData {
-    throws: (boolean | null)[]
+  throws: (boolean | null)[];
 }
 
 export const SavingThrow: FunctionComponent<ISavingThrowData> = (props) => {
-    return (
-        <div className="flex flex-col items-center">
-                <span className="text-sm">Death Saves</span>
-                <div className="flex flex-row justify-center">
-                    {
-                        props.throws[0]
-                        ? <Icon icon="fa6-solid:cross" className="drop-shadow-[0px_0px_2px_#F6E3B9]"/>
-                        : <Icon icon="mdi:grave-stone"/>
-                    }
-                    {
-                        props.throws[1]
-                        ? <Icon icon="fa6-solid:cross" className="drop-shadow-[0px_0px_2px_#F6E3B9]"/>
-                        : <Icon icon="mdi:grave-stone"/>
-                    }
-                    {
-                        props.throws[2]
-                        ? <Icon icon="fa6-solid:cross" className="drop-shadow-[0px_0px_2px_#F6E3B9]"/>
-                        : <Icon icon="mdi:grave-stone"/>
-                    }
-                </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col items-center">
+      <span className="text-sm">Death Saves</span>
+      <div className="flex flex-row justify-center">
+        {props.throws.map((value, key) => {
+          return value === null ? (
+            <Icon icon="bi:dot" key={key} />
+          ) : (
+            <Icon
+              icon={value ? "fa6-solid:cross" : "mdi:grave-stone"}
+              className={value ? "drop-shadow-[0px_0px_2px_#F6E3B9]" : ""}
+              key={key}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
