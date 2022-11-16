ad28a6ce58c0e90c193c33cea92f449db5e17a7e
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-15 21:11:21 -0300

    refactor: Add forwarded ref and extend props

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,48 +1,50 @@
-import { NextComponentType } from "next";
-import { HTMLInputTypeAttribute } from "react";
+import {
+  DetailedHTMLProps,
+  ForwardedRef,
+  forwardRef,
+  InputHTMLAttributes,
+} from "react";
 import { twMerge } from "tailwind-merge";
-interface IInputProps {
+interface IInputProps
+  extends DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
   label?: string;
-  name: string;
-  type: HTMLInputTypeAttribute;
-  className?: string;
   errorClassName?: string;
-  onChange?: (...args: any) => any;
   error?: {
     message?: string;
   };
-  value?: string;
 }
 
-export const Input = ({
-  label,
-  name,
-  type,
-  className,
-  errorClassName = "border-b-2 border-danger-500",
-  onChange,
-  error,
-  value,
-}: IInputProps) => {
-  return (
-    <label className="flex flex-col text-medium text-left text-primary-500 mt-2 w-full">
-      {label}
-      <input
-        className={twMerge(
-          "bg-black/30 text-white border-b-2 border-primary-500 px-2 sm:w-80 sm:h-9 w-60 rounded-lg focus:outline-none",
-          className,
-          error?.message ? errorClassName : ""
+export const Input = forwardRef(
+  (_props: IInputProps, ref: ForwardedRef<HTMLInputElement>) => {
+    const {
+      label,
+      errorClassName = "border-b-2 border-danger-500",
+      error,
+      className,
+      ...props
+    } = _props;
+
+    return (
+      <label className="flex flex-col text-medium text-left text-primary-500 mt-2 w-full">
+        {label}
+        <input
+          className={twMerge(
+            "bg-black/30 text-white border-b-2 border-primary-500 px-2 sm:w-80 sm:h-9 w-60 rounded-lg focus:outline-none",
+            className,
+            error?.message ? errorClassName : ""
+          )}
+          {...props}
+          ref={ref}
+        />
+        {error?.message && (
+          <div className="text-xs font-light text-danger-500">
+            {error.message}
+          </div>
         )}
-        type={type}
-        name={name}
-        onChange={onChange}
-        value={value}
-      />
-      {error?.message && (
-        <div className="text-xs font-light text-danger-500">
-          {error.message}
-        </div>
-      )}
-    </label>
-  );
-};
+      </label>
+    );
+  }
+);
