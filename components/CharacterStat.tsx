edff56360c646433ffb2150495c8a6eed9112df2
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-15 21:02:08 -0300

    feat: Add getModifier function

--- components/CharacterStat.tsx
+++ components/CharacterStat.tsx
@@ -1,18 +1,23 @@
 import { FunctionComponent } from "react";
 
 interface ICharacterStatData {
-    name: string
-    value: number
+  name: string;
+  value: number;
 }
 
+const getModifier = (stat: number) => Math.floor((stat - 10) / 2);
+
 export const CharacterStat: FunctionComponent<ICharacterStatData> = (props) => {
-    return (
-        <div className="flex flex-col items-center px-1 md:p-2">
-            <span className="uppercase font-bold text-md drop-shadow-[0px_0px_2px_#F6E3B9]">{props.name}</span>
-            <div className="flex flex-row text-xs">
-                <span>{props.value}</span>
-                <span className="font-thin">({((props.value - 10) / 2) | 0})</span>
-            </div>
-        </div>
-    )
-}
+  const modifier = getModifier(props.value);
+  return (
+    <div className="flex flex-col items-center px-1 md:p-2">
+      <span className="uppercase font-bold text-md drop-shadow-[0px_0px_2px_#F6E3B9]">
+        {props.name}
+      </span>
+      <div className="flex flex-row text-xs gap-1">
+        <span>{props.value}</span>
+        <span className="font-thin">({modifier})</span>
+      </div>
+    </div>
+  );
+};
