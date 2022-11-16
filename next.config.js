b16fd75717945940696bb1e3e13d51a8fd106f47
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-15 21:00:52 -0300

    chore: Add picsum image domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["i.picsum.photos"],
+    formats: ["image/avif", "image/webp"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
