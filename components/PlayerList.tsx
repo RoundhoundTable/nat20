2945d853e73dfedf2dfcea34225d2c2e8e916adc
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-15 21:11:36 -0300

    feat: Get players from room

--- components/PlayerList.tsx
+++ components/PlayerList.tsx
@@ -1,17 +1,30 @@
 import { NextComponentType } from "next";
 import { PlayerDisplay } from "./PlayerDisplay";
-import { twMerge } from "tailwind-merge";
+import useGame from "../hooks/useGame";
 
 export const PlayerList: NextComponentType = () => {
-    return (
-        <div className="overflow-auto w-full md:w-1/2 h-1/3 md:h-full flex flex-row md:flex-col gap-3">
-            <PlayerDisplay picture="https://picsum.photos/100" hitPoints={16} name="Azariah Ravawraek" _class="Sorcerer" level={3} CA={14} currentHitPoints={8}/>
-            <PlayerDisplay picture="https://picsum.photos/100" hitPoints={16} name="Azariah Ravawraek" _class="Sorcerer" level={3} CA={14} currentHitPoints={2}/>
-            <PlayerDisplay picture="https://picsum.photos/100" hitPoints={16} name="Azariah Ravawraek" _class="Sorcerer" level={3} CA={14} currentHitPoints={0}/>
-            <PlayerDisplay picture="https://picsum.photos/100" hitPoints={16} name="Azariah Ravawraek" _class="Sorcerer" level={3} CA={14} currentHitPoints={10}/>
-            <PlayerDisplay picture="https://picsum.photos/100" hitPoints={16} name="Azariah Ravawraek" _class="Sorcerer" level={3} CA={14} currentHitPoints={10}/>
-            <PlayerDisplay picture="https://picsum.photos/100" hitPoints={16} name="Azariah Ravawraek" _class="Sorcerer" level={3} CA={14} currentHitPoints={16}/>
-            <PlayerDisplay picture="https://picsum.photos/100" hitPoints={16} name="Azariah Ravawraek" _class="Sorcerer" level={3} CA={14} currentHitPoints={10}/>
-        </div>
-    )
-}
+  const { room } = useGame();
+  return (
+    <div className="overflow-auto w-full md:w-1/2 h-1/3 md:h-full flex flex-row md:flex-col gap-3">
+      {room?.players ? (
+        Object.keys(room.players).map((id, key) => {
+          const player = room.players[id];
+          return (
+            <PlayerDisplay
+              picture={player.picture}
+              hitPoints={player.hitPoints}
+              name={player.name}
+              _class={player.class}
+              level={player.level}
+              CA={player.classArmor}
+              currentHitPoints={player.currentHp}
+              key={key}
+            />
+          );
+        })
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
