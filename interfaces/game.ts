b2e61751eaceac6b2e8d4f6c958aae92ffce52bc
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-15 21:04:08 -0300

    feat: Add interfaces for message and room

---
+++ interfaces/game.ts
@@ -0,0 +1,16 @@
+import { EMessages } from "../enums/messages";
+import { ICharacter } from "./entities";
+
+export interface IMessage {
+  role: EMessages;
+  name?: string;
+  thumbnail?: string;
+  message: string;
+}
+
+export interface IRoomState {
+  id: string;
+  dungeonMaster: string;
+  messages: EMessages[];
+  players: Record<string, ICharacter>;
+}
