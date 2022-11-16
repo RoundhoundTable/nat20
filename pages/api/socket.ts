bd5e6654f348293b21ac868b6d281d2c52498b83
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-15 21:06:59 -0300

    feat: Implement socket events

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,7 +1,16 @@
 import { Server, Socket } from "socket.io";
-import createRoom from "../../modules/sockets/handlers/createRoom";
-import disconnect from "../../modules/sockets/handlers/disconnect";
-import joinRoom from "../../modules/sockets/handlers/joinRoom";
+import { EVENTS } from "../../enums/events";
+import RoomHandler from "../../libs/roomHandler";
+import { customAlphabet } from "nanoid";
+import Room from "../../modules/sockets/classes/Room";
+import prisma from "../../libs/prisma";
+import { ICharacter } from "../../interfaces/entities";
+import { IMessage } from "../../interfaces/game";
+import { EMessages } from "../../enums/messages";
+
+const genId = customAlphabet(
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+);
 
 export default function handler(req: any, res: any) {
   if (res.socket.server.io) {
@@ -13,11 +22,91 @@ export default function handler(req: any, res: any) {
   res.socket.server.io = io;
 
   const onConnection = async (socket: Socket) => {
-    socket.on("joinRoom", joinRoom);
-    socket.on("createRoom", createRoom);
-    socket.on("sendMessage", () => {});
-    socket.on("newMessage", () => {});
-    socket.on("disconnect", disconnect);
+    socket.on(EVENTS.CREATE_ROOM, ({ password }) => {
+      const id = genId(8);
+
+      const room = new Room(id, password, socket);
+
+      RoomHandler.rooms.set(id, room);
+      RoomHandler.socketRooms.set(socket, room);
+
+      socket.emit(EVENTS.CONNECTED_TO_ROOM, room.get());
+    });
+
+    socket.on(EVENTS.JOIN_ROOM, async ({ id, password, characterId }) => {
+      const room = RoomHandler.rooms.get(id);
+
+      if (!room) throw Error("Not Found");
+
+      let character = (await prisma.character.findUnique({
+        where: {
+          id: characterId,
+        },
+      })) as ICharacter;
+
+      if (!character) throw Error("Character not found");
+
+      character.currentHp = character.hitPoints;
+      character.death_throws = [null, null, null];
+
+      const joinResult = await room.join(socket, password, character);
+
+      if (!joinResult) return false;
+
+      RoomHandler.socketRooms.set(socket, room);
+
+      io.sockets.in(room.id).emit(EVENTS.CONNECTED_TO_ROOM, room.get());
+    });
+
+    socket.on(EVENTS.UPDATE_CHARACTER, ({ id, payload }) => {
+      const room = RoomHandler.socketRooms.get(socket);
+
+      if (!room) return false;
+
+      room.updateCharacter(id, payload);
+
+      io.sockets.in(room.id).emit(EVENTS.REFRESH_UI, room.get());
+    });
+
+    socket.on(EVENTS.SEND_MESSAGE, (message: string) => {
+      const room = RoomHandler.socketRooms.get(socket);
+
+      if (!room) return false;
+
+      let messageObj: IMessage = {
+        role:
+          room.dungeonMaster?.id === socket.id
+            ? EMessages.DM
+            : EMessages.PLAYER,
+        message,
+      };
+
+      if (messageObj.role === EMessages.PLAYER) {
+        messageObj.name = room.players.get(socket.id)?.name;
+        messageObj.thumbnail = room.players.get(socket.id)?.picture;
+      }
+
+      io.sockets.in(room.id).emit(EVENTS.MESSAGE, messageObj);
+    });
+
+    socket.on(EVENTS.DISCONNECTING, () => {
+      const room = RoomHandler.socketRooms.get(socket);
+
+      if (!room) return;
+
+      const character = room.players.get(socket.id);
+
+      room.players.delete(socket.id);
+      RoomHandler.socketRooms.delete(socket);
+
+      let messageObj: IMessage = {
+        role: EMessages.BOT,
+        message: `${character?.name} ha salido de la sala`,
+      };
+
+      io.sockets.in(room.id).emit(EVENTS.USER_LEFT, messageObj);
+      io.sockets.in(room.id).emit(EVENTS.REFRESH_UI, room.get());
+    });
   };
 
   io.on("connection", onConnection);
