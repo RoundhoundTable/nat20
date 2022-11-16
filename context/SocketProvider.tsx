09af9d2e111893f2256a0b7082c700e902f4bd73
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-15 21:08:25 -0300

    feat: Add client socket events

--- context/SocketProvider.tsx
+++ context/SocketProvider.tsx
@@ -4,11 +4,12 @@ import io from "socket.io-client";
 import { ISocketContext } from "../interfaces/context";
 import { useAuth } from "../hooks/useAuth";
 import useGame from "../hooks/useGame";
+import { EVENTS } from "../enums/events";
 
 export const SocketContext = createContext<ISocketContext | null>(null);
 
 const SocketProvider = ({ children }: { children: ReactNode }) => {
-  const gameContext = useGame();
+  const { messages, setMessages, room, setRoom } = useGame();
   const { currentUser } = useAuth();
   const [socket, setSocket] = useState<Socket | null>(null);
 
@@ -16,20 +17,50 @@ const SocketProvider = ({ children }: { children: ReactNode }) => {
     await fetch("/api/socket");
     const _socket = io();
 
-    _socket.on("roomConnected", (args) => {
-      gameContext.setIsDm(args.isDm);
-      gameContext.setRoomId(args.room);
+    _socket.on(EVENTS.CONNECTED_TO_ROOM, (args) => {
+      setRoom(args);
     });
+
+    _socket.on(EVENTS.REFRESH_UI, (args) => {
+      setRoom(args);
+    });
+
+    _socket.on(EVENTS.MESSAGE, (message) =>
+      setMessages((state) => [
+        ...state,
+        {
+          ...message,
+        },
+      ])
+    );
+
+    _socket.on(EVENTS.USER_LEFT, (message) =>
+      setMessages((state) => [
+        ...state,
+        {
+          ...message,
+        },
+      ])
+    );
 
     setSocket(_socket);
   };
 
   const createRoom = (password: string) => {
-    if (currentUser && socket) socket.emit("createRoom", password);
+    if (currentUser && socket) socket.emit(EVENTS.CREATE_ROOM, { password });
   };
 
-  const joinRoom = (id: string, password: string) => {
-    if (currentUser && socket) socket.emit("joinRoom", { id, password });
+  const joinRoom = (id: string, password: string, characterId: string) => {
+    console.log({
+      event: EVENTS.JOIN_ROOM,
+      data: { id, password, characterId },
+    });
+    if (currentUser && socket)
+      socket.emit(EVENTS.JOIN_ROOM, { id, password, characterId });
+  };
+
+  const sendMessage = (message: string) => {
+    if (socket) socket.emit(EVENTS.SEND_MESSAGE, message);
   };
 
   const value = {
@@ -37,6 +68,7 @@ const SocketProvider = ({ children }: { children: ReactNode }) => {
     initializeSocket,
     joinRoom,
     createRoom,
+    sendMessage,
   };
 
   return (
