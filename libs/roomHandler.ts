ffe2e69974f0fe152d0b939a4a65b1f9b42b2940
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-15 21:02:27 -0300

    refactor: Change SocketHandler to RoomHandler

--- libs/roomHandler.ts
+++ libs/roomHandler.ts
@@ -1,9 +1,9 @@
 import { Socket } from "socket.io";
 import Room from "../modules/sockets/classes/Room";
 
-class SocketHandler {
+class RoomHandler {
   public static readonly rooms = new Map<string, Room>();
   public static readonly socketRooms = new Map<Socket, Room>();
 }
 
-export default SocketHandler;
+export default RoomHandler;
