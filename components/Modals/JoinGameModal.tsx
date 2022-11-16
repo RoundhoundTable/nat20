49e2b796c5cd0bcd3afb3a9bd304c560435b42c8
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-15 21:10:36 -0300

    feat: Add JoinGame functionality

--- components/Modals/JoinGameModal.tsx
+++ components/Modals/JoinGameModal.tsx
@@ -1,15 +1,33 @@
 import { NextComponentType } from "next";
 import { Input } from "../Input";
 import Button from "../Button";
+import { createRef } from "react";
+import { useSocket } from "../../hooks/useSocket";
 
 const JoinGameModal: NextComponentType = () => {
+  const { joinRoom } = useSocket();
+  const passwordRef = createRef() as React.MutableRefObject<HTMLInputElement>;
+  const idRef = createRef() as React.MutableRefObject<HTMLInputElement>;
+
+  const handleJoin = () => {
+    joinRoom(idRef.current.value, passwordRef.current.value, "1");
+  };
+
   return (
-    <div className="flex flex-col items-center gap-5">
+    <form
+      className="flex flex-col items-center gap-5"
+      onSubmit={(ev: any) => ev.preventDefault()}
+    >
       <p className="text-2xl font-bold uppercase text-primary-500">Join Game</p>
-      <Input name="id" label="Game ID" type={"text"} />
-      <Input name="password" label="Password" type={"password"} />
-      <Button>Join</Button>
-    </div>
+      <Input ref={idRef} name="id" label="Game ID" type={"text"} />
+      <Input
+        ref={passwordRef}
+        name="password"
+        label="Password"
+        type={"password"}
+      />
+      <Button onClick={handleJoin}>Join</Button>
+    </form>
   );
 };
 
