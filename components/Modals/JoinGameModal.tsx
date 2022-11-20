074f767c50220efd3832835389dd8df52adc8677
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-19 23:43:03 -0300

    feat: Add character select to join game modal

--- components/Modals/JoinGameModal.tsx
+++ components/Modals/JoinGameModal.tsx
@@ -1,24 +1,32 @@
 import { NextComponentType } from "next";
 import { Input } from "../Input";
 import Button from "../Button";
-import { createRef } from "react";
+import React, { createRef, ElementRef, useRef } from "react";
 import { useSocket } from "../../hooks/useSocket";
 import useModal from "../../hooks/useModal";
+import CharacterSelect from "../CharacterSelect";
+
+type CharacterSelectHandle = ElementRef<typeof CharacterSelect>;
 
 const JoinGameModal: NextComponentType = () => {
   const { joinRoom } = useSocket();
   const { unsetModal } = useModal();
+  const selectRef = useRef<CharacterSelectHandle>(null);
   const passwordRef = createRef() as React.MutableRefObject<HTMLInputElement>;
   const idRef = createRef() as React.MutableRefObject<HTMLInputElement>;
 
   const handleJoin = () => {
-    joinRoom(idRef.current.value, passwordRef.current.value, "1");
+    joinRoom(
+      idRef.current.value,
+      passwordRef.current.value,
+      selectRef.current?.selected.id as string
+    );
     unsetModal();
   };
 
   return (
     <form
-      className="flex flex-col items-center gap-5"
+      className="flex flex-col items-center gap-5 overflow-visible"
       onSubmit={(ev: any) => ev.preventDefault()}
     >
       <p className="text-2xl font-bold uppercase text-primary-500">Join Game</p>
@@ -29,6 +37,7 @@ const JoinGameModal: NextComponentType = () => {
         label="Password"
         type={"password"}
       />
+      <CharacterSelect ref={selectRef} />
       <Button onClick={handleJoin}>Join</Button>
     </form>
   );
