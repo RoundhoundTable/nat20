99e84bafdf542ac5e0966ed6831f0ca8b7aeafc4
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-19 23:41:56 -0300

    feat: Add wrapperClassName & hideDelete props

--- components/Character.tsx
+++ components/Character.tsx
@@ -1,5 +1,6 @@
 import { Icon } from "@iconify/react";
 import { FunctionComponent } from "react";
+import { twMerge } from "tailwind-merge";
 import { EModals } from "../enums/modals";
 import { useDeleteCharacter } from "../hooks/useDeleteCharacter";
 import useModal from "../hooks/useModal";
@@ -10,6 +11,9 @@ interface ICharacterData {
   name: string;
   _class: string;
   level: number;
+  hideDelete?: boolean;
+  wrapperClassname?: string;
+  onClick?: (...args: any) => void;
 }
 
 export const Character: FunctionComponent<ICharacterData> = ({
@@ -18,21 +22,32 @@ export const Character: FunctionComponent<ICharacterData> = ({
   name,
   _class,
   level,
+  hideDelete,
+  wrapperClassname,
+  onClick,
 }) => {
   const { setModal } = useModal();
   const { deleteCharacter } = useDeleteCharacter(id);
   return (
-    <div className="flex flex-row gap-4 items-center p-2 bg-background-600/50 snap-start text-primary-500 relative">
-      <button
-        onClick={() =>
-          setModal(EModals.CONFIRMATION, {
-            message: `¿Está seguro que desea eliminar a ${name}?`,
-            accept: deleteCharacter,
-          })
-        }
-      >
-        <Icon icon="akar-icons:cross" className="absolute top-4 right-2" />
-      </button>
+    <div
+      className={twMerge(
+        "flex flex-row gap-4 items-center p-2 bg-background-600/50 snap-start text-primary-500 relative",
+        wrapperClassname
+      )}
+      onClick={onClick}
+    >
+      {!hideDelete && (
+        <button
+          onClick={() =>
+            setModal(EModals.CONFIRMATION, {
+              message: `¿Está seguro que desea eliminar a ${name}?`,
+              accept: deleteCharacter,
+            })
+          }
+        >
+          <Icon icon="akar-icons:cross" className="absolute top-4 right-2" />
+        </button>
+      )}
       <img
         src={picture}
         className="w-14 rounded-full object-cover aspect-square"
