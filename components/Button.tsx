498c4eb23cd02a7f729553d7f8b28f2ac413b096
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-20 19:21:46 -0300

    feat: Add disabled styles

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, FunctionComponent, ReactNode } from "react";
+import { ButtonHTMLAttributes, FunctionComponent } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
@@ -12,7 +12,7 @@ const Button: FunctionComponent<ButtonProps> = ({
     <button
       {...props}
       className={twMerge(
-        "bg-primary-500 sm:w-80 sm:h-9 w-60 rounded-lg uppercase border border-primary-500 hover:bg-background-500 text-background-500 hover:text-primary-500 text-xl",
+        "bg-primary-500 sm:w-80 sm:h-9 w-60 rounded-lg uppercase border border-primary-500 hover:bg-background-500 text-background-500 hover:text-primary-500 disabled:hover:text-background-500 text-xl disabled:bg-background-400 disabled:border-background-600",
         className
       )}
     >
