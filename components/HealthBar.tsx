f2c74333a930f4a81124d2bfc2a189d2eac8ae4d
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-24 00:57:15 -0300

    feat: Add extra healthbar for health overload

--- components/HealthBar.tsx
+++ components/HealthBar.tsx
@@ -1,20 +1,40 @@
 import { FunctionComponent } from "react";
 
 interface IHealthBarData {
-    hitPoints: number
-    currentHitPoints: number
+  hitPoints: number;
+  currentHitPoints: number;
 }
 
 export const HealthBar: FunctionComponent<IHealthBarData> = (props) => {
-    return (
-        <div className="relative w-full h-5 bg-background-500 rounded-lg shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
-            <div className={`h-full bg-danger-800 rounded-lg`} style={
-                {
-                    width: `${ ( 100 / props.hitPoints ) * props.currentHitPoints }%`
-                }
-            }>
-                <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">{props.currentHitPoints}/{props.hitPoints}</span>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="relative w-full h-5 bg-background-500 rounded-lg shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
+      <div
+        className={`h-full bg-danger-800 rounded-lg`}
+        style={{
+          width: `clamp(0%, ${
+            (100 / props.hitPoints) * props.currentHitPoints
+          }%, 100%)`,
+        }}
+      >
+        {props.currentHitPoints > props.hitPoints ? (
+          <div
+            className={`h-full bg-warning-500 rounded-lg`}
+            style={{
+              width: `clamp(0%, ${
+                (100 / props.hitPoints) * props.currentHitPoints - 100
+              }%, 100%)`,
+            }}
+          >
+            <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              {props.currentHitPoints}/{props.hitPoints}
+            </span>
+          </div>
+        ) : (
+          <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            {props.currentHitPoints}/{props.hitPoints}
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
