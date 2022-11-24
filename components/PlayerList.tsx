66248d162ac006a49e644869fdecbf9b32cd8c32
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-24 01:00:17 -0300

    refactor: Add PlayerDisplay missing props and make full height

--- components/PlayerList.tsx
+++ components/PlayerList.tsx
@@ -1,16 +1,19 @@
 import { NextComponentType } from "next";
 import { PlayerDisplay } from "./PlayerDisplay";
 import useGame from "../hooks/useGame";
+import { useSocket } from "../hooks/useSocket";
 
 export const PlayerList: NextComponentType = () => {
+  const { socket } = useSocket();
   const { room } = useGame();
   return (
-    <div className="overflow-auto w-full md:w-1/2 h-1/3 md:h-full flex flex-row md:flex-col gap-3 items-center">
+    <div className="overflow-y-hidden md:overflow-auto w-full bg-background-600/50 rounded-xl md:mt-9 md:w-1/2 md:min-w-max flex flex-row md:flex-col gap-3 items-center p-5">
       {room?.players ? (
         Object.keys(room.players).map((id, key) => {
           const player = room.players[id];
           return (
             <PlayerDisplay
+              id={id}
               picture={player.picture}
               hitPoints={player.hitPoints}
               name={player.name}
@@ -20,6 +23,7 @@ export const PlayerList: NextComponentType = () => {
               currentHitPoints={player.currentHp}
               deathThrows={player.death_throws}
               key={key}
+              isDm={socket?.id === room?.dungeonMaster}
             />
           );
         })
