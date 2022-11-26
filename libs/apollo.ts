58653d415b923e05f6e4a09dee21e9e513002916
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-25 22:43:08 -0300

    feat: Add error formatting

--- libs/apollo.ts
+++ libs/apollo.ts
@@ -4,6 +4,8 @@ import typeDefs from "../modules/graphql/schema";
 import Mutation from "../modules/graphql/resolvers/mutation";
 import Query from "../modules/graphql/resolvers/query";
 import { createContext } from "../modules/graphql/context";
+import { GraphQLError } from "graphql";
+import { randomUUID } from "crypto";
 
 const apollo: ApolloServer = new ApolloServer({
   typeDefs,
@@ -12,6 +14,22 @@ const apollo: ApolloServer = new ApolloServer({
     Mutation,
   },
   context: createContext,
+  formatError(error: GraphQLError) {
+    if (error.originalError instanceof GraphQLError) return error;
+
+    const errorId = randomUUID();
+    console.error({
+      error: {
+        id: errorId,
+        name: error.name,
+        message: error.message,
+        path: error.path,
+        stack: error.stack,
+      },
+    });
+
+    return new GraphQLError(`Internal Error: ${errorId}`);
+  },
   plugins: [
     ApolloServerPluginLandingPageGraphQLPlayground({
       settings: {
